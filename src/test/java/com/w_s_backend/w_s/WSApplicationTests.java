fd4e7ac615ceea12dfc57c60355f7a8852eb6d02
SergeyGT <sergeyburdenyuk@mail.ru>
2025-11-19 12:11:53 +0300

    Initialize project

---
+++ src/test/java/com/w_s_backend/w_s/WSApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w_s_backend.w_s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
