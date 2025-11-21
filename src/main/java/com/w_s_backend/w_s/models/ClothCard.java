49ec65574e512d6d1b6320663b7177d11643b6ca
SergeyGT <sergeyburdenyuk@mail.ru>
2025-11-21 13:42:49 +0300

    Update: Добавлена модель для записи в БД

---
+++ src/main/java/com/w_s_backend/w_s/models/ClothCard.java
@@ -0,0 +1,36 @@
+package com.w_s_backend.w_s.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class ClothCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "cloth_name", nullable = false)
+    private String clothName;
+
+    @Column(name = "image_path")
+    private String imagePath;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private ClothingCategory category; 
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "season")
+    private Season season;
+
+    @Column(name = "warmth_level")
+    private Integer warmthLevel;
+
+    @Column(name = "color")
+    private String color;
+}
---
+++ src/main/java/com/w_s_backend/w_s/models/Season.java
@@ -0,0 +1,8 @@
+package com.w_s_backend.w_s.models;
+
+public enum Season {
+    SUMMER,
+    AUTUMN,
+    SPRING,
+    WINTER    
+}
